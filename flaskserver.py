93cab98ed02d5113d5f94edfd3e1399bf7a6630b
Sandro <0771090100za@gmail.com>
2025-05-20 15:54:24 +0200

    Progress First Lesson

--- flaskserver.py
+++ flaskserver.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")  # Verbindet HTML mit dieser Route
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    print(f"Benutzername: {username}")
+    print(f"Passwort: {password}")
+    return f"Login empfangen für Benutzer: {username}"
 
 if __name__ == "__main__":
     app.run(debug=True)
